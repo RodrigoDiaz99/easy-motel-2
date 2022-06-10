57eb29edecab2e41517526620d2c9ad96630c9f2
Rodrigo Diaz Serviran <diaz-rodrigo@hotmail.com>
2022-06-09 23:12:02 -0500

    Reparacion proyecto

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,26 +13,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
-	return view('pages.welcome');
-})->name('welcome');
+    return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('dashboard', 'HomeController@index')->name('home');
-Route::get('pricing', 'PageController@pricing')->name('page.pricing');
-Route::get('lock', 'PageController@lock')->name('page.lock');
-
-Route::group(['middleware' => 'auth'], function () {
-	Route::resource('category', 'CategoryController', ['except' => ['show']]);
-	Route::resource('tag', 'TagController', ['except' => ['show']]);
-	Route::resource('item', 'ItemController', ['except' => ['show']]);
-	Route::resource('role', 'RoleController', ['except' => ['show', 'destroy']]);
-	Route::resource('user', 'UserController', ['except' => ['show']]);
-
-	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
-	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
-
-	Route::get('{page}', ['as' => 'page.index', 'uses' => 'PageController@index']);
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
